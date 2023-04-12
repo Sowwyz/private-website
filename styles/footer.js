0f011856090f38999acfcd5375f1b7b72ffdfa3b
Sowwyz <88189918+Sowwyz@users.noreply.github.com>
2023-04-12 21:33:07 +0300

    Add files via upload

---
+++ styles/footer.js
@@ -0,0 +1,33 @@
+
+function footer(){
+    document.write(`\
+        <div id="footer-box">\
+        \
+            <div id="footer">\
+                <div id="footer-infos">\
+                    <div>\
+                        <h2>Sowwyz</h2>\
+                        <p>\
+                           <br>\
+                            Sowwyz#1337<br>\
+                            
+                        </p>\
+                    </div>\
+                    <div id="footer-icons">\
+                      
+                     </br>
+                        <a href="https://github.com/Sowwyz" target="_blank" class="fa fa-github"></a>\
+                      
+                    </div>\
+                </div>\
+        \
+                <div id="footer-date">\
+                    <p>${new Date().getFullYear()}</p>\
+                </div>\
+            </div>\
+        \
+        </div>\
+    `);
+}
+/// Sowwyz#1337 ///
+/// Sowwyz ///
